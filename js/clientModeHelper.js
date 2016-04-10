3d9d44b7d5f5204ccb8e82176db83f1150994652
Caducas <nikolaus@famkofler.com>
2016-04-10 17:32:40 +0200

    Revert "no message"
    This reverts commit a20ec3756c33051ba20ba073a7219b8e4cecc6ad.

--- js/clientModeHelper.js
+++
@@ -1,9 +0,0 @@
-var nodeWebkitMode = (typeof process == "object");
-
-if(isNodeWebkit) {
-  var configHelper = require(process.cwd() + '/ConfigHelper');
-}
-
-function isNodeWebkit() {
-	return nodeWebkitMode;
-}
