c3feb07cfef2976cc9936ff7866f2c44fb481025
Caducas <nikolaus@famkofler.com>
2017-05-07 11:52:04 +0200

    moved configuration of username/password (using localstorage) into own helper

--- www/js/ConfigHelper.js
+++ www/js/ConfigHelper.js
@@ -1,22 +1,18 @@
 
 function configHelper() {
-
-	this.getUsername = function() {
-		return localStorage.getItem("username");		
+	return {
+		getUsername: function() {
+			return localStorage.getItem("username");		
+		}, 
+		getPassword: function() {
+			return localStorage.getItem("password");
+		}, 
+		setUsername: function(username) {
+			localStorage.setItem("username", username);
+		}, 
+		setPassword: function(password) {
+			var passwordEncrypted = CryptoJS.SHA512(password).toString(CryptoJS.enc.Hex);
+			localStorage.setItem("password", passwordEncrypted);
+		}
 	}
-
-	this.getPassword = function() {
-		return localStorage.getItem("password");
-	}
-
-	this.setUsername = function(username) {
-		localStorage.setItem("username", username);
-	}
-
-	this.setPassword = function(password) {
-		var passwordEncrypted = CryptoJS.SHA512(password).toString(CryptoJS.enc.Hex);
-		localStorage.setItem("password", passwordEncrypted);
-	}
-
-	return this;
 }
