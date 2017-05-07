f52b41d1416f0c8c9ccd747c4509b28f150f9758
Caducas <nikolaus@famkofler.com>
2017-05-07 11:58:43 +0200

    fixed usage of config helper

--- www/js/ConfigHelper.js
+++ www/js/ConfigHelper.js
@@ -1,18 +1,16 @@
 
-function configHelper() {
-	return {
-		getUsername: function() {
-			return localStorage.getItem("username");		
-		}, 
-		getPassword: function() {
-			return localStorage.getItem("password");
-		}, 
-		setUsername: function(username) {
-			localStorage.setItem("username", username);
-		}, 
-		setPassword: function(password) {
-			var passwordEncrypted = CryptoJS.SHA512(password).toString(CryptoJS.enc.Hex);
-			localStorage.setItem("password", passwordEncrypted);
-		}
+var configHelper = {
+	getUsername: function() {
+		return localStorage.getItem("username");		
+	}, 
+	getPassword: function() {
+		return localStorage.getItem("password");
+	}, 
+	setUsername: function(username) {
+		localStorage.setItem("username", username);
+	}, 
+	setPassword: function(password) {
+		var passwordEncrypted = CryptoJS.SHA512(password).toString(CryptoJS.enc.Hex);
+		localStorage.setItem("password", passwordEncrypted);
 	}
 }
